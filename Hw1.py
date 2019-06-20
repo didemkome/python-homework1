252f2eda2d471fb90b69c41e75be5b1f22998e3d
didmkme <didemkome@gmail.com>
2019-06-20 17:22:56 +0300

    add print function

--- Hw1.py
+++ Hw1.py
@@ -8,9 +8,11 @@ def extract_data(filename):
             if name not in student_data:
                 student_data[name] = []
             student_data[name].append(score)
-            print(student_data[name])
         return (student_data.items())
 
 data  = extract_data('C:/Users/46921166678/Python-Homework1/info.txt')
 highest_averages = sorted(data, key=lambda x: sum(x[1])/float(len(x[1])), reverse=True)
 highest_scores = sorted(data, key=lambda x: max(x[1]), reverse=True)
+
+def print_function(x, y):
+    print("{}, {}".format(x, y))
