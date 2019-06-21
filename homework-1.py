b1b576deabdba7ff9917bfba3e41ce55cc3c2d7a
didmkme <didemkome@gmail.com>
2019-06-21 15:44:04 +0300

    change info.txt utf-8

--- homework-1.py
+++ homework-1.py
@@ -21,7 +21,7 @@ def extract_data(filename):
             student_data[name].append(score)
         return (student_data.items())
 
-data  = extract_data('C:/Users/46921166678/Python-Homework1/info.txt')
+data  = extract_data('./info.txt')
 highest_averages = sorted(data, key=lambda x: sum(x[1])/float(len(x[1])), reverse=True)
 highest_scores = sorted(data, key=lambda x: max(x[1]), reverse=True)
 
@@ -39,12 +39,12 @@ def print_function(x, y):
     '''
     print("{}, {}".format(x, y))
 
-if __name__ == '__main__':
-    import doctest
-    doctest.testmod()
-
 for name, score in highest_scores:
     print_function(name, max(score))
 
 for name, score in highest_averages:
     print_function(name, sum(score)/float(len(score)))
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
